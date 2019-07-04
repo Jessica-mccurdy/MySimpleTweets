a5511bd2c707297cfda09caf25b036e5def08569
jessica-mccurdy <jessica.mccurdy@yale.edu>
2019-07-03 17:30:53 -0700

    Can post messages!

--- build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/mediacompat/R.java
+++ build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/mediacompat/R.java
@@ -117,7 +117,7 @@ public final class R {
         public static final int text = 0x7f07008f;
         public static final int text2 = 0x7f070090;
         public static final int time = 0x7f070093;
-        public static final int title = 0x7f070094;
+        public static final int title = 0x7f070095;
     }
     public static final class integer {
         private integer() {}
--- src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+//import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -18,6 +20,8 @@ import org.json.JSONObject;
 import org.parceler.Parcels;
 
 import java.util.ArrayList;
+
+
 
 import cz.msebera.android.httpclient.Header;
 
@@ -140,6 +144,27 @@ public class TimelineActivity extends AppCompatActivity {
                                }
         );
     }
+
+    /*
+    // getRelativeTimeAgo("Mon Apr 01 21:16:23 +0000 2014");
+    public String getRelativeTimeAgo(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        String relativeDate = "";
+        try {
+            long dateMillis = sf.parse(rawJsonDate).getTime();
+            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return relativeDate;
+    }
+    */
+
 }
 
 
